d4236911144d17c4d7c70f8475c8597662903c4a
Purushothaman <ram.purushothaman65@gmail.com>
2021-06-01 10:00:04 +0530

    build home page with multiple components finished homepage styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Diractory from "../../components/diractory/diractory.component";
+
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Diractory />
+  </div>
+);
+
+export default HomePage;
