ac8fdb695d098b4da100a16f1626097001a10063
Purushothaman <ram.purushothaman65@gmail.com>
2021-06-08 13:30:56 +0530

    updated package.json with route lib, added route feature in index.js and app.js file updated router

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/next" component={HatsPage} />
+        <Route exact path="/hats" component={HatsPage} />
       </Switch>
     </div>
   );
